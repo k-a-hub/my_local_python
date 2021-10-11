5f9887d20f9be8bea4943c604288dd021b22a597
wif_adm_ka.araki <katsuya-araki@froide.co.jp>
2021-10-11 17:31:06 +0900

    雛形追加

---
+++ tamaya/main.py
@@ -0,0 +1,27 @@
+
+# ここから開始
+if __name__ == '__main__':
+    
+    # dtb_orderの受注一覧を取得
+    # 絞り込み条件は進物受付 かつ シーズン内の期間のデータ
+
+    # dtb_shippingの受注のお届け先一覧を取得
+    # 検索条件はdtb_orderのorder_id
+    # MEMO: 1受注のお届け先情報を取得
+
+    # dtb_customer_addressの一覧を取得
+    # 条件なし
+
+    # dtb_customer_address と dtb_shipping の突き合わせ
+    
+        # 名前の一致するか？
+            # 一致の場合、updateリストに追加
+            # 不一致の場合、insertリストに追加
+
+
+    # dtb_customer_addressへの反映
+    # updateリストの更新
+    # insertリストの追加
+
+    # 処理終了
+    exit
