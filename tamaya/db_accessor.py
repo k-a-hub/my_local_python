bb7406dde8efb0e95179e002ac5dec3f8aff4fb8
wif_adm_ka.araki <katsuya-araki@froide.co.jp>
2021-10-13 15:27:07 +0900

    #3 mysql接続機能追加 docker設定の変更、DB、python環境も修正 db_accessor.pyの追加

---
+++ tamaya/db_accessor.py
@@ -0,0 +1,51 @@
+import mysql.connector as mysql
+
+
+class db_accessor:
+
+    # mysql接続情報
+    # Dockerで作成した別コンテナのmysql環境に接続する情報
+    config = {
+        'host': 'tamaya_db'
+        , 'port': '3306'
+        , 'user': 'test'
+        , 'password': 'password'
+        , 'database': 'tamaya'
+        , 'auth_plugin': 'mysql_native_password'
+    }
+
+    # コンストラクタ
+    def __init__(self):
+
+        try:
+            # mysqlに接続
+            self.conn = mysql.connect(**self.config)
+            # コネクションが切れた時に再接続してくれるよう設定
+            self.conn.ping(reconnect=True)
+            # 自動コミット設定をオフ
+            self.conn.autocommit = False
+
+            # カーソル情報の初期化
+            self.conn.is_connected()
+            self.cur = self.conn.cursor()
+        except Exception as e:
+            print(f"connect error: \r\n{e}")
+            
+    # デストラクタ
+    def __del__(self):
+
+        try:
+            self.conn.close()
+        except Exception as e:
+            print(f"connect close error: \r\n{e}")
+
+    # クエリ実行
+    def execute_query(self, sql):
+        try:
+            # クエリ実行
+            self.cur.execute(sql)
+            result = self.cur.fetchall()
+            return result
+        except Exception as e:
+            print(f"query error: \r\n{e}")
+
--- tamaya/main.py
+++ tamaya/main.py
@@ -1,14 +1,68 @@
+import db_accessor as dba
+
+# 受注注文日
+order_date = "2021-12-31 00:00:00"
+
 
 # ここから開始
 if __name__ == '__main__':
+
+    # DB接続クラス作成
+    db_accessor = dba.db_accessor()
     
     # dtb_order と dtb_shipping を id と order_id　で結合
     # 絞り込み条件は進物受付 かつ シーズン内の期間のデータ
     # MEMO: 1受注のお届け先情報リスト
+    order_shipping_select_sql = f"""
+    select
+	    dtb_order.customer_id as "会員ID"
+	    ,dtb_order.id as "注文番号"
+        ,dtb_order_item.accept_no as "承り票番号"
+        ,dtb_shipping.phone_number
+        ,dtb_shipping.postal_code
+        ,dtb_shipping.addr01
+        ,dtb_shipping.addr02
+        ,dtb_shipping.company_name
+        ,dtb_shipping.name01
+        ,dtb_shipping.name02
+    from
+        dtb_shipping
+        ,dtb_order
+        ,dtb_order_item
+    where
+	    dtb_shipping.order_id = dtb_order.id
+        and	dtb_shipping.order_id = dtb_order_item.order_id
+        and	dtb_order.order_date > '{order_date}'
+        and	dtb_order_item.product_code is not null
+        and	dtb_order.payment_id IN(7, 8, 9)
+    group by
+        dtb_order.customer_id
+        ,dtb_order.id
+        ,dtb_order_item.accept_no
+        ,dtb_shipping.phone_number
+        ,dtb_shipping.postal_code
+        ,dtb_shipping.addr01
+        ,dtb_shipping.addr02
+        ,dtb_shipping.company_name
+        ,dtb_shipping.name01
+        ,dtb_shipping.name02
+    """
+
+    order_shipping_list = db_accessor.execute_query(order_shipping_select_sql)
+    print(order_shipping_list)
 
     # dtb_customer_addressの一覧を取得
     # 条件なし
     # MEMO: 依頼主のお届け先情報リスト
+    customer_address_sql = f"""
+    select
+        *
+    from
+        dtb_customer_address
+    """
+
+    customer_address_list = db_accessor.execute_query(customer_address_sql)
+    print(customer_address_list)
 
     # 「dtb_order と dtb_shippingの結合」リスト と dtb_customer_addressリスト の突き合わせ
     
