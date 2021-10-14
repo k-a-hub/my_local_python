161942e1df67cbb47606526f2926098dcfbf417b
wif_adm_ka.araki <katsuya-araki@froide.co.jp>
2021-10-14 13:28:09 +0900

    #12 upsert判断処理 dtb_customer_address.pyの追加

---
+++ tamaya/dtb_customer_address.py
@@ -0,0 +1,20 @@
+
+class dtb_customer_address:
+
+    # コンストラクタ
+    def __init__(self):
+
+        # 更新リスト
+        self.update_list = []
+        # 登録リスト
+        self.insert_list = []
+    
+    # 更新リスト追加
+    def add_update_list(self, order_shipping, customer_address):
+        # 更新する受注のお届け先情報を追加
+        self.update_list.append(order_shipping)
+
+    # 登録リスト追加
+    def add_insert_list(self, order_shipping):
+        # 登録する受注のお届け先情報を追加
+        self.insert_list.append(order_shipping)
